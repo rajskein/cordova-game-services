aa02d4892355cae2dcb822a3cd914f7b56cf97af
kingemerald <rajamancikam.r@skeintech.com>
2019-05-03 13:18:53 +0530

    updated the plugin

--- src/com/skein/cordova/plugins/BaseGameActivity.java
+++ src/com/skein/cordova/plugins/BaseGameActivity.java
@@ -1,18 +1,4 @@
-/*
- * Copyright (C) 2013 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 
 package com.skein.cordova.plugins;
 
@@ -23,23 +9,6 @@ import android.util.Log;
 
 import com.google.android.gms.common.api.GoogleApiClient;
 
-/**
- * Example base class for games. This implementation takes care of setting up
- * the API client object and managing its lifecycle. Subclasses only need to
- * override the @link{#onSignInSucceeded} and @link{#onSignInFailed} abstract
- * methods. To initiate the sign-in flow when the user clicks the sign-in
- * button, subclasses should call @link{#beginUserInitiatedSignIn}. By default,
- * this class only instantiates the GoogleApiClient object. If the PlusClient
- * is also wanted, call the BaseGameActivity(int)
- * constructor and specify the requested clients. For example, to request
- * PlusClient and GamesClient, use BaseGameActivity(CLIENT_GAMES | CLIENT_PLUS).
- * To request all available clients, use BaseGameActivity(CLIENT_ALL).
- * Alternatively, you can also specify the requested clients via
- * @link{#setRequestedClients}, but you must do so before @link{#onCreate}
- * gets called, otherwise the call will have no effect.
- *
- * @author Bruno Oliveira (Google)
- */
 public abstract class BaseGameActivity extends FragmentActivity implements
         GameHelper.GameHelperListener {
 
